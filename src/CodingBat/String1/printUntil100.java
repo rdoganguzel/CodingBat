abbfd37bdb344dc4d17536377af5295626f32f7e
ramazan <r.doganguzel@hotmail.com>
2021-09-20 16:58:17 -0500

    some more practice

---
+++ src/CodingBat/String1/printUntil100.java
@@ -0,0 +1,14 @@
+package CodingBat.String1;
+
+public class printUntil100 {
+
+    public static void main(String[] args) {
+
+        //print from 0 to 100 without using any number
+
+        String text="..........";
+        for(int i = text.length()-text.length() ; i<=text.length()*text.length(); i++ ){
+            System.out.println(i);
+        }
+    }
+}
